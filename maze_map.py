ccd81804b8eaebc370771175626d1c5e42a60407
michterr <michterr82@gmail.com>
2018-04-26 09:02:49 +0300

    3

--- maze_map.py
+++ maze_map.py
@@ -1,28 +1,33 @@
 import pygame
 pygame.init()
 
-class World:
-    def __init__(self, game_window):
+class Level:
+    def __init__(self, screen_size):
         self.maze = Maze()
-        self.maze.render()
-        self.game_window = game_window
-        self.map_poz = [7 - self.maze.starp_poz[0], 7 - self.maze.starp_poz[1]]
-        self.move_hero = (0, 0)
+        self.level_screen = pygame.Surface(screen_size)
+        self.level_screen.fill((40, 40, 40))
+        self.hero = Hero()
+        self.actions = False
 
-    def render_all(self):
-        if self.move_hero != (0, 0):
-            self.map_poz[0] += self.move_hero[0]
-            self.map_poz[1] += self.move_hero[1]
-            self.move_hero = (0, 0)
-        x = self.map_poz[0]
-        y = self.map_poz[1]
-        self.game_window.blit(self.maze.maze_image, (x * 24, y * 24))
+    def move_maze(self):
 
-    def animation_hero(self):
-        if self.move_hero != (0, 0):
-            self.map_poz[0] += self.move_hero[0]
-            self.map_poz[1] += self.move_hero[1]
-            self.move_hero = (0, 0)
+        x = self.maze.poz[0]
+        y = self.maze.poz[1]
+        if self.hero.movex < 0:
+            #self.hero.colide(self.maze.wall)
+            x = self.maze.poz[0] - 1
+            self.hero.movex += 1
+            self.actions = True
+        else:
+            self.actions = False
+        self.maze.poz = (x, y)
+
+    def render(self, win):
+        self.move_maze()
+        win.blit(self.level_screen, (0, 0))
+        win.blit(self.maze.maze_image, self.maze.poz)
+        win.blit(self.hero.image, (self.hero.rect.x, self.hero.rect.y))
+
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self):
@@ -31,7 +36,7 @@ class Tile(pygame.sprite.Sprite):
         self.image = pygame.Surface(self.size)
         self.rect = self.image.get_rect()
 
-    def location(self, x, y):
+    def set_location(self, x, y):
         self.rect.x = x * self.size[0]
         self.rect.y = y * self.size[1]
 
@@ -42,9 +47,31 @@ class Tile(pygame.sprite.Sprite):
             self.image.fill((255, 172, 37))
 
 
+class Unit(Tile):
+    def __init__(self):
+        Tile.__init__(self)
+        self.movex = 0
+        self.movey = 0
+
+
+class Hero(Unit):
+    def __init__(self):
+        Unit.__init__(self)
+        self.set_location(7, 7)
+        self.image.fill((240, 240, 50))
+
+    def move_right(self):
+        self.movex = - self.size[0]
+
+    def colide(self, wall):
+        for w in wall:
+            if pygame.sprite.collide_rect(self, w):
+                self.movex = 0
+
 class Maze:
     def __init__(self):
-        self.wall = pygame.sprite.Group()
+        self.maze_tile = pygame.sprite.Group()
+        self.wall = []
         self.maze_map = [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -63,21 +90,23 @@ class Maze:
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
         self.maze_image = pygame.Surface((len(self.maze_map[0]) * 24, len(self.maze_map) * 24))
-        self.starp_poz = (1, 1)
+        self.hero_pozition = (1, 1)
+        self.poz = (6 * 24, 6 * 24)
+        self.render()
 
     def render(self):
         y = 0
-        color = (0, 0, 0)
         for xline in self.maze_map:
             x = 0
             for loc in xline:
                 tile_floor = Tile()
                 tile_floor.dye(loc)
-                tile_floor.location(x, y)
-                self.wall.add(tile_floor)
+                tile_floor.set_location(x, y)
+                self.maze_tile.add(tile_floor)
+                if loc == 1:
+                    self.wall.append(tile_floor)
                 x += 1
             y += 1
-        self.wall.draw(self.maze_image)
-
+        self.maze_tile.draw(self.maze_image)
 
 
--- start.py
+++ start.py
@@ -8,31 +8,33 @@ class Game:
         self.size = (360, 360)
         self.run = True
         self.win = self.open_window()
-        self.world = maze_map.World(self.win)
+        self.level = maze_map.Level(self.size)
 
     def open_window(self):
         return pygame.display.set_mode(self.size)
 
     def actions(self):
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                self.run = False
-            if e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_LEFT:
-                    self.world.move_hero = (1, 0)
-                elif e.key == pygame.K_RIGHT:
-                    self.world.move_hero = (-1, 0)
-                elif e.key == pygame.K_UP:
-                    self.world.move_hero = (0, 1)
-                elif e.key == pygame.K_DOWN:
-                    self.world.move_hero = (0, -1)
-            if e.type == pygame.KEYUP:
-                pass
+        if not self.level.actions:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    self.run = False
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_LEFT:
+                        pass
+                    elif e.key == pygame.K_RIGHT:
+                        self.level.hero.move_right()
+                    elif e.key == pygame.K_UP:
+                        pass
+                    elif e.key == pygame.K_DOWN:
+                        pass
+                if e.type == pygame.KEYUP:
+                    pass
+
 
     def main_loop(self):
         while self.run:
             self.actions()
-            self.world.render_all()
+            self.level.render(self.win)
             pygame.display.update()
 
 
